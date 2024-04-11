c070bbe3f105354c5968cc99d4ee94557766c31e
Duc Pham <duc.pham01@platform.inc>
2024-04-11 10:36:42 +0700

    add inside ver

--- src/api/version.ts
+++ src/api/version.ts
@@ -3,17 +3,21 @@ import express from 'express';
 const router = express.Router();
 
 type VersionResponse = {
-    version: string;
-    name: string;
+  version: string;
+  name: string;
 }[];
 
 router.get<{}, VersionResponse>('/', (req, res) => {
-    res.json([{
-        version: "0.0.8",
-        name: "policy"
-    },{
-        version: "0.0.9",
-        name: "booking"
+  res.json([
+    {
+      version: '0.0.8',
+      name: 'policy',
+    }, {
+      version: '0.0.9',
+      name: 'booking',
+    }, {
+      version: '0.0.1',
+      name: 'inside',
     }]);
 });
 
